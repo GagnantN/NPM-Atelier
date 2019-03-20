6bc9317510ed922d7154a8c7bb7a760b0799936e
Gagnant Nicolas <gagnantnicolas64@gmail.com>
2019-03-20 17:52:27 +0100

    atelier wilders for npm

---
+++ wilders.js
@@ -0,0 +1,6 @@
+const chalk = require ("chalk");
+
+console.log(chalk.bgRed.bold('Babtiste'));
+console.log(chalk.bgMagenta.bold("Laura"));
+console.log(chalk.bgBlue.bold('Nizar'));
+console.log(chalk.bgGreen.bold('Yann'));
